65a5a3e4b71acf5533d4ff540dfca6fbb1258f87
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2022-02-24 20:34:06 -0600

    what i got done in 40min

--- src/frontend/src/pages/ViewOrdersPage.js
+++ src/frontend/src/pages/ViewOrdersPage.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 export default function ViewOrdersPage() {
+    const [orders, setOrders] = useState([])
+    async function getOrders () {
+        const response = await fetch("/api/v1/orders")
+        const data = await response.json()
+        setOrders(data)
+    }
+    useEffect(() =>{
+        getOrders()
+    },[])
     return (
         <div>
             On the ViewOrdersPage
+            <ul>
+            {
+                orders.map(o => {
+                    return <li>{o.id}</li>
+                })
+            }
+            </ul>
         </div>
     )
 }
